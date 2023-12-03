6ade85764c177b97734c9708844d61521c9bb900
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-12-03 17:52:12 +0100

    Added urls to products app

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+# products/urls.py
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+    path('', views.all_products, name='all_products'),
+    path('category/<slug:category_slug>/', views.all_products, name='products_by_category'),
+    
+]
