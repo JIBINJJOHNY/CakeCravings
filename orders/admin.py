09f8986d87d5bda53fb81d81ee702e65890fc583
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-12-04 15:37:45 +0100

    Register order model and orderitem model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
+# admin.py
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1  # Number of empty forms to display for adding related OrderItem objects
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_id', 'full_name', 'email', 'created', 'status']
+    inlines = [OrderItemInline]
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'size']
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
