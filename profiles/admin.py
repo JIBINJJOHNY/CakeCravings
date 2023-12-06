e08e064987b92d08bdefc53b8e3484a86a0f1257
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-12-06 19:31:50 +0100

    Register profiles model

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'first_name',
+        'last_name',
+        'birthday',
+        'avatar',
+        'subscription'
+    )
+    list_filter = (
+        'user',
+        'first_name',
+        'last_name',
+        'birthday',
+        'avatar',
+        'subscription'
+    )
+    search_fields = (
+        'user',
+        'first_name',
+        'last_name',
+        'birthday',
+        'avatar',
+        'subscription'
+    )
